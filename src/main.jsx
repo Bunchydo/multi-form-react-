0f72bfc6040935f97bf18f7ac0ae417dd9789ba1
Roy <royelpemberton@gmail.com>
2025-09-21 21:13:16 -0400

    final version

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import FormContainer from './form-holder.jsx'
+import './resetSheet.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
